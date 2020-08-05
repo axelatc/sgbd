f92e4bf264d1cbc807be1658e0c7e1a32a857914
axelatc <axel.krebs@promsocatc.net>
2020-08-06 01:33:48 +0200

    Corrige l url du servlet de démonstration

--- src/be/atc/servlet/ServletAtc.java
+++ src/be/atc/servlet/ServletAtc.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "/ServletAtc")
+@WebServlet("/ServletAtc")
 public class ServletAtc extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
