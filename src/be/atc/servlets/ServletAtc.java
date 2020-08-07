87b11be0972c3887560b859e31842572c7693b3f
axelatc <axel.krebs@promsocatc.net>
2020-08-07 19:35:49 +0200

    Fix outdated bug in mysql connector

--- src/be/atc/servlets/ServletAtc.java
+++ src/be/atc/servlets/ServletAtc.java
@@ -1,5 +1,7 @@
 package be.atc.servlets;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -8,7 +10,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/ServletAtc")
 public class ServletAtc extends HttpServlet {
@@ -17,8 +23,24 @@ public class ServletAtc extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaSGBD");
-        EntityManager em = emf.createEntityManager();
+        InitialContext ctx = null;
+        try {
+            ctx = new InitialContext();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        try {
+            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/jpaDS");
+            try {
+                Connection conn = ds.getConnection();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        //        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaSGBD");
+//        EntityManager em = emf.createEntityManager();
 //        RolesEntity role = new RolesEntity();
 //        System.out.println("avant peristence nouveau role");
 //        //role.setId(5);
