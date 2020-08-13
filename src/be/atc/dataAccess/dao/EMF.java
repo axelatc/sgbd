9dfd9d66b6b210225c590b006d9c90f2eece6e42
axelatc <axel.krebs@promsocatc.net>
2020-08-13 20:32:21 +0200

    Add Diana's Entity Manager classes

---
+++ src/be/atc/dataAccess/dao/EMF.java
@@ -0,0 +1,35 @@
+package be.atc.dataAccess.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Class to get a connection to the database
+ *
+ * @author Renaud DIANA
+ */
+public final class EMF {
+
+    private static EntityManagerFactory emfInstance =
+            Persistence.createEntityManagerFactory("your_unit_persistence_name");
+
+    private EMF() {}
+
+    public static EntityManagerFactory getEMF() {
+        return emfInstance;
+    }
+
+    public static EntityManager getEM() {
+        return emfInstance.createEntityManager();
+    }
+
+    /*	Create EntityManager in others classes
+     * EntityManager em = EMF.getEM();
+     * try {
+     *     // ... do stuff with em ...
+     * } finally {
+     *     em.close();
+     * }
+     */
+}
