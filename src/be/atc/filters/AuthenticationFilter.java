a178c1fe3917c074478b76cc1ce2d92a48bd0259
axelatc <axel.krebs@promsocatc.net>
2020-08-19 22:33:49 +0200

    Add redirect to /login if none session is found for the client

--- src/be/atc/filters/AuthenticationFilter.java
+++ src/be/atc/filters/AuthenticationFilter.java
@@ -2,19 +2,44 @@ package be.atc.filters;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(filterName = "AuthenticationFilter")
 public class AuthenticationFilter implements Filter {
+    @Override
     public void destroy() {
+        // TODO Auto-generated method stub
     }
 
+    @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp);
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response= (HttpServletResponse) resp;
+
+        String incomingPath = request.getRequestURI().substring( request.getContextPath().length() );
+
+        // All clients can access static files
+        if (incomingPath.startsWith( "/resources" )) {
+            chain.doFilter( request, response );
+            return;
+        }
+
+        // Only authentified clients can access the app
+        HttpSession session= request.getSession();
+        if(session.getAttribute("currentUser") != null || incomingPath.equalsIgnoreCase("/login")) {
+            chain.doFilter(request, response);
+        } else {
+            response.sendRedirect(request.getContextPath() + "/login");
+        }
+
     }
 
+    @Override
     public void init(FilterConfig config) throws ServletException {
-
+        // TODO Auto-generated method stub
     }
 
 }
