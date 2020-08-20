ea82b27e86caa2624504db5445948b97179f1194
axelatc <axel.krebs@promsocatc.net>
2020-08-20 23:54:22 +0200

    Add processing and validation of login form in Servlet

--- src/be/atc/servlets/ServletLogin.java
+++ src/be/atc/servlets/ServletLogin.java
@@ -1,9 +1,14 @@
 package be.atc.servlets;
 
+import be.atc.dataAccess.EMF;
+import be.atc.dataAccess.entities.WorkersEntity;
+import be.atc.dataAccess.services.WorkersService;
+import be.atc.models.UserModel;
 import be.atc.test.servlets.ServletTestLogin;
 import be.atc.utils.AppConfig;
 import org.apache.log4j.Logger;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,7 +31,59 @@ public class ServletLogin extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("User attempts to login");
+        Collection<String> errorMessages = new ArrayList<String>();
+        String forwardURL = "";
 
+        String requestURI = request.getRequestURI();
+        if (requestURI.endsWith("/login")) {
+            String inputUsername = request.getParameter("username");
+            String inputPassword = request.getParameter("password");
+            UserModel user = new UserModel(inputUsername, inputPassword);
+
+            log.debug("Credentials inputted " + "username: " + inputUsername + " password: " + inputPassword);
+            // Les données du formulaires sont-elles correctes?
+            if (user.loginIsValid()
+                    && user.passwordIsValid()) {
+                EntityManager em = EMF.getEM();
+                WorkersEntity worker = new WorkersService(em).findWorkerByUsernameOrNull(inputUsername);
+                // L'utilisateur existe dans la DB
+                if (worker != null) {
+                    // Le mot de passe entré est correct
+                    if (inputPassword.equals(worker.getPasswordKey())) {
+                        request.getSession().setAttribute("authUser", worker);
+                        log.debug("Authenticated user: " + worker.toString());
+                        forwardURL = "/home";
+
+                    }
+                    // Le mdp est incorrect
+                    else {
+                        errorMessages.add("Veuillez réessayer.");
+                        forwardURL = VIEWS_PATHS.get("login");
+                    }
+                }
+                // L'utilisateur n'existe pas dans la DB
+                else {
+                    errorMessages.add("Votre nom d'utilisateur est introuvable.");
+                    forwardURL = VIEWS_PATHS.get("login");
+                }
+            }
+            // Données du formulaire incorrectes
+            else {
+                errorMessages.add("Veuillez entrer un nom d'utilisateur et un mot de passe valides.");
+                forwardURL = VIEWS_PATHS.get("login");
+            }
+        }
+
+        StringBuilder errorMessagesBuilder = new StringBuilder();
+        for(String s : errorMessages) {
+            errorMessagesBuilder.append(s);
+        }
+        request.setAttribute("errors", errorMessagesBuilder.toString());
+
+        getServletContext()
+                .getRequestDispatcher(forwardURL)
+                .forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
